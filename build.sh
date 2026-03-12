2ef633e07461dfd2b342a74d140840446efda456
Chanchanon Rangpeung <SuperDoggez2004@gmail.com>
2026-03-12 22:37:38 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 
-if ["$DJANGO_SUPERUSER_USERNAME"] then
+if ["$DJANGO_SUPERUSER_USERNAME"]; then
     python manage.py createsuperuser \
         --no-input \
         --username $DJANGO_SUPERUSER_USERNAME \
-        --email $DJANGO_SUPERUSER_EMAIL \
+        --email $DJANGO_SUPERUSER_EMAIL 
 fi
